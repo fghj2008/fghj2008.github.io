bc9db64c4f5ecbd08e722ec757cd551c5de9025c
fghj2008 <31117468+fghj2008@users.noreply.github.com>
2017-08-27 20:00:40 +0800

    45

--- js-1/nav.js
+++ js-1/nav.js
@@ -3,7 +3,7 @@ var bars = document.getElementById("nav-action");
 var nav = document.getElementById("nav");
 var area1_1 = document.getElementById("area1_1");
 var area1_2 = document.getElementById("area1_2");
-var footer = document.getElementById("footer");
+// var footer = document.getElementById("footer");
 
 //setting up the listener
 bars.addEventListener("click", barClicked, false);
@@ -16,5 +16,5 @@ function barClicked() {
 
   area1_1.classList.toggle('z-index-hidden');
   area1_2.classList.toggle('z-index-hidden');
-  footer.classList.toggle('#nav-1');
+  // footer.classList.toggle('#nav-1');
 }
